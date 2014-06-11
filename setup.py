50d2639145709dd35cb7c841538b1c5829b7046a
Daniel Lawrence <dannyla@linux.com>
2014-06-11 22:51:17 +1000

    added basic tests

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(
+    name="ExternalNaginator",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Daniel Lawrence",
+    author_email="dannyla@linux.com",
+    description="Generate nagios configuration from puppetdb",
+    license="MIT",
+    keywords="puppetdb nagios",
+    url="http://github.com/daniellawrence/external_naginator",
+)
