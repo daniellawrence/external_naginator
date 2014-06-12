89b49a4c611978728a963d4a10d436498b6cc6dd
Daniel Lawrence <dannyla@linux.com>
2014-06-12 23:56:30 +1000

    setup.py to include main file as script

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
+
 setup(
     name="ExternalNaginator",
     version="0.0.1",
@@ -9,6 +9,7 @@ setup(
     author="Daniel Lawrence",
     author_email="dannyla@linux.com",
     description="Generate nagios configuration from puppetdb",
+    scripts=["generate.py"],
     license="MIT",
     keywords="puppetdb nagios",
     url="http://github.com/daniellawrence/external_naginator",
